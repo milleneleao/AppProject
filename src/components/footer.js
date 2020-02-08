04a95c97ef9ec026c983285ed988761a19b56c6d
Millene Leao <milleneleao@gmail.com>
2020-02-08 18:56:55 -0500

    footer' changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import About from '../About';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 import '../scss/custom.scss';
-import * as serviceWorker from '../serviceWorker';
 
-function Footer() {
+export default function Footer() {
   return (
     <div className=" bg-light" style={{position: "absolute", bottom: "0",width: "100%",height: "2.5rem"}}>
-      <div className="row  pt-2">
-        <div className="col-6 ">
-        <Link to
-          to="../About"
-            activeClassName="About">
-            About
-          </Link>
-          <Route
-                    path="/About"
-                    component={About} 
-                />
-          <a href="www.google.com" className="text-project-color-2 pl-5">About Project</a>
-        </div>
-        <div className="col-6 text-right ">
-          <p className="text-project-color-2 pr-5">&copy;2020 - Group2.</p>
-        </div>
+    <div className="row  pt-2">
+      <div className="col-3 ">
+        <Link to="/about" className="pr-5">About</Link>
+        <Link to="/project">About the Project</Link>
+      </div>
+      <div className="col-6 text-right ">
+        <p className="text-project-color-2 pr-5">&copy;2020 - Group2.</p>
       </div>
     </div>
+      </div>
   );
 }
 
-export default Footer;
+//export default Footer;
+// function Footer() {
+//   return (
+//     <div className=" bg-light" style={{position: "absolute", bottom: "0",width: "100%",height: "2.5rem"}}>
+//       <div className="row  pt-2">
+//         <div className="col-6 ">
+    
+//           <a href="www.google.com" className="text-project-color-2 pl-5">About Project</a>
+//         </div>
+//         <div className="col-6 text-right ">
+//           <p className="text-project-color-2 pr-5">&copy;2020 - Group2.</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import About from './About';
-import Welcome from './Welcome';
 import './scss/custom.scss';
+import Welcome from './Welcome';
+import About from './About';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<Welcome />, document.getElementById('welcome'));
-//ReactDOM.render(<About />, document.getElementById('about'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Welcome} />
+            <Route path="/about" component={About} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
+//
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Welcome from './Welcome';
+// import './scss/custom.scss';
+// import * as serviceWorker from './serviceWorker';
+
+
+
+// ReactDOM.render(<Welcome />, document.getElementById('root'));
 serviceWorker.unregister();
